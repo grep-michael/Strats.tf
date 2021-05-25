7f6a11d29bd73ca3ac49af3335cfba7562974fff
michaelknudsen <mmknudsen@bsu.edu>
2021-05-25 11:59:34 -0400

    begun working on route for getting map name

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var {v4:uuidv4} = require('uuid') 
-
+var {Client,Room} = require('../db/index.js');
+const client = new Client();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index.html');
 });
+
 router.get('/makeRoom/:mapName', function(req, res, next) {
   //create new room
   //res.statusCode = 302;
-  res.setHeader('map',req.params.mapName);
-  res.location(`/${uuidv4()}`)
-  //res.set({
-  //  'map':req.params.mapName
-  //})
   
-  console.log(res)
-  res.status(400).end()
-  //res.redirect(`/${uuidv4()}`);
-  
-  
-  
-});
+  let room = new Room(uuidv4(),req.params.mapName);
+  client.addRoom(room)
 
+  res.location(`/${room.getID()}`)
+  res.status(302).end()
+
+});
+router.get('/getRoom/:roomId', function(req, res, next) {
+  let id = req.params.roomId
+  console.log('begin search')
+  x = client.roomFromDBWhereIDEquals(id)
+  console.log('done with search')
+  res.json({room:x});
+})
 //router.get('/draw', function(req, res, next) {
 //  res.render('draw.html',{title:"Starts.tf"});
 //});
 
 //join room
 router.get('/:roomId', function(req, res, next) {
-  console.log(req.headers)
   res.render('draw.html',{ROOMID:req.params.roomId,MapName:NaN});
 });
 
